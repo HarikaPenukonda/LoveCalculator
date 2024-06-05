29943c5f5796f44ea524265ac2fe0cfbb6bc4eb4
HarikaPenukonda <penukondaharika@gmail.com>
2024-06-05 12:20:45 -0500

    binding the values

--- loveCalculator/src/main/java/com/seleniumexpress/lc/controllers/LCAppController.java
+++ loveCalculator/src/main/java/com/seleniumexpress/lc/controllers/LCAppController.java
@@ -3,6 +3,8 @@ package com.seleniumexpress.lc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.seleniumexpress.lc.api.RegisterInfo;
 import com.seleniumexpress.lc.api.UserInfo;
 
 @Controller
@@ -17,6 +19,11 @@ public class LCAppController {
 	@RequestMapping("/process-homepage")
 	public String showResult(@ModelAttribute("userInfo") UserInfo userInfo) {
 		return "show-result";
+	}
+	
+	@RequestMapping("/register-user")
+	public String registerUser(@ModelAttribute("registerInfo") RegisterInfo registerInfo) {
+		return "register-success";
 	}
 
 }
